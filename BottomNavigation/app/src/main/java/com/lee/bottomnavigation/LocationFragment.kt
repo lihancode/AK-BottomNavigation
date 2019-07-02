0dfaf0c3a1f3fd3e0f9dac1a4215a16bc9d4e701
LeeChen <encorex32268@gmail.com>
2019-07-02 12:45:08 +0900

    screenshot

--- BottomNavigation/app/src/main/java/com/lee/bottomnavigation/LocationFragment.kt
+++ BottomNavigation/app/src/main/java/com/lee/bottomnavigation/LocationFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.menu.MenuBuilder
 import androidx.appcompat.widget.Toolbar
 import kotlinx.android.synthetic.main.fragment_location.*
 
@@ -14,14 +15,29 @@ class LocationFragment : Fragment() {
     companion object{
         fun newInstance() = LocationFragment()
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        setHasOptionsMenu(true)
         return inflater.inflate(R.layout.fragment_location, container, false)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
+
+        inflater.inflate(R.menu.location_menu,menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if (item?.itemId==R.id.ic_location_location){
+            showToast("Location Finding ... ")
+            return true;
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 
 
